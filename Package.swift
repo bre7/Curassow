5d5c9736ce400ac2e462a625aa24bad02fdb45df
bre7 <bre7@users.noreply.github.com>
2015-12-30 20:16:00 -0300

    Custom fork

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
     .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 4),
+    .Package(url: "https://github.com/bre7/Commander.git", majorVersion: 0, minor: 4),
   ],
   testDependencies: [
     .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 6),
